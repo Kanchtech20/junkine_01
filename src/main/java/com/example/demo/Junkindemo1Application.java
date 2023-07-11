1db826d0f3ed09df9f6d08289c41023cbb53b6e4
kanchan shelar <kanchtech20@gmail.com>
2023-07-11 16:31:33 +0530

    FIle created

---
+++ src/main/java/com/example/demo/Junkindemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junkindemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junkindemo1Application.class, args);
+	}
+
+}
